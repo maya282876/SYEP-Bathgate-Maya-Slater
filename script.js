7d77b70daf717df10691652034d6618818395c6a
maya282876 <mayaslater716@gmail.com>
2025-08-05 15:52:22 +0000

    new

--- script.js
+++ script.js
@@ -1,3 +1,116 @@
+const reviews = [
+  {
+    id: 1, 
+    name: 'Susan smith',
+    job: 'Web Developer',
+    img: 'farm1.jpeg',
+    text: 'filler text',
+  },
+
+
+   {
+    id: 2, 
+    name: 'Susan smith',
+    job: 'Web Developer',
+    img: '#',
+    text: 'filler text',
+},
+
+  {
+    id: 3, 
+    name: 'Susan smith',
+    job: 'Web Developer',
+    img: '#',
+    text: 'filler text',
+  },
+];
+// select items
+const img = document.getElementById('person-img');
+const author = document.getElementById('author');
+const job = document.getElementById('job');
+const info = document.getElementById('info');
+
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+
+let currentItem = 0;
+
+window.addEventListener('DOMContentLoaded' , function(){
+const item = reviews[currentItem];
+img.src = item.img;
+author.textContent = item.name;
+job.textContent = item.job;
+info.textContent = item.text;
+});
+
+function showPerson(person){
+  const item = reviews [person];
+img.src = item.img;
+author.textContent = item.name;
+job.textContent = item.job;
+info.textContent = item.text;
+}
+
+nextBtn.addEventListener('click', function (){
+    currentItem++;
+    if(currentItem > reviews.length - 1){
+      currentItem = 0;
+    }
+    showPerson(currentItem);
+});
+
+prevBtn.addEventListener('click', function (){
+    currentItem--;
+    if(currentItem < 0) {
+      currentItem = reviews.length - 1;
+    }
+    showPerson(currentItem);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // console.log("HELLO WORLD!");
 
 // let myString = "a2b3c";
